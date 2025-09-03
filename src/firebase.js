f51ca8fd089788bba7fee9110343a2cccb8f4d6e
omchunamari <chunamariom@gmail.com>
2025-09-03 15:01:35 +0530

    asdad

--- src/firebase.js
+++ src/firebase.js
@@ -2,18 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAniCpNDkEKFuzC4akLDLtyQgczRLUo4tc",
-  authDomain: "fir-auth-8ba5e.firebaseapp.com",
-  projectId: "fir-auth-8ba5e",
-  storageBucket: "fir-auth-8ba5e.appspot.com",
-  messagingSenderId: "1067238815703",
-  appId: "1:1067238815703:web:cba599cf5a54f0641fbd92",
-  measurementId: "G-MSCWGW42DW"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
-
 export default app;
